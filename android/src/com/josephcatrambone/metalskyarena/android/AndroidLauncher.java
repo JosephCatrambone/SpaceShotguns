fd503cf29c60739b6219e1ab173085e5015e28f7
unknown <jo.jcat@gmail.com>
2015-12-20 13:35:02 -0800

    Initial commit.

---
+++ android/src/com/josephcatrambone/metalskyarena/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.josephcatrambone.metalskyarena.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.josephcatrambone.metalskyarena.MainGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MainGame(), config);
+	}
+}
---
+++ desktop/src/com/josephcatrambone/metalskyarena/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.josephcatrambone.metalskyarena.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.josephcatrambone.metalskyarena.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
