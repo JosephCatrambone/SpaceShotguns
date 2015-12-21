82bf36369d9361ec57290307c6aa35583a7ec564
unknown <jo.jcat@gmail.com>
2015-12-20 17:22:08 -0800

    Add player + scene swapping.

--- core/src/com/josephcatrambone/metalskyarena/MainGame.java
+++ core/src/com/josephcatrambone/metalskyarena/MainGame.java
@@ -2,26 +2,34 @@ package com.josephcatrambone.metalskyarena;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+import com.josephcatrambone.metalskyarena.scenes.PlayScene;
+import com.josephcatrambone.metalskyarena.scenes.Scene;
+
+import java.util.Stack;
 
 public class MainGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	public static final World world;
+	public static final AssetManager assetManager;
+	public static final Stack<Scene> scenes;
+
+	static {
+		world = new World(new Vector2(0, 0), true);
+		assetManager = new AssetManager();
+		scenes = new Stack<>();
+	}
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		scenes.push(new PlayScene());
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		float dt = Gdx.graphics.getDeltaTime();
+		scenes.peek().update(dt);
+		scenes.peek().render(dt);
 	}
 }
---
+++ core/src/com/josephcatrambone/metalskyarena/PhysicsConstants.java
@@ -0,0 +1,8 @@
+package com.josephcatrambone.metalskyarena;
+
+/**
+ * Created by Jo on 12/20/2015.
+ */
+public class PhysicsConstants {
+	public static final float PPM = 10.0f; // Pixels per meter. Ten pixels = 1m.
+}
